194e470ba003db235d1b36c762917bff26e326ca
Collin Guillaume <220053@CCM-Asty-Moulin.local>
2023-04-21 16:14:43 +0200

    el

--- puissance4/puissance4/Program.cs
+++ puissance4/puissance4/Program.cs
@@ -6,7 +6,18 @@ namespace puissance4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[,] grille = new string[6,7];//grille de jeux
+            bool player;//bool qui montre quelle joueur joue
+            int n;// numero de la colone dans laquelle le joueur veut rentrez son pion 
+            int compt=1;//compteur de coup joue  
+            outils outils = new outils();
+            outils.rempliGrille(out grille);
+            do
+            {
+                outils.affichgrille(grille);
+                Console.WriteLine(compt);
+                compt = compt + 1;
+            } while (compt<43);
         }
     }
 }
---
+++ puissance4/puissance4/outils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace puissance4
+{
+    class outils
+    {
+        public void rempliGrille(out string[,] grille)
+        {
+            grille = new string[6,7];
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++)
+
+                    grille[i,j] = "0";
+
+            }
+        }
+        public void affichgrille(string[,] grille)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    if (grille[i,j]=="0")
+                    {
+                        Console.Write(grille[i, j]);
+                    }
+                    else if (grille[i, j] == "O")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(grille[i, j]);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write(grille[i, j]);
+                    }
+                }
+                Console.WriteLine("");
+            }
+
+        }
+    }
+}
