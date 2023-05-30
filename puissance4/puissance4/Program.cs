c9be9bf3e7ebeb17bee37700ad110781958f09f6
guillaumecollin <220053@site.asty-moulin.be>
2023-05-30 22:47:26 +0200

    lol

--- puissance4/puissance4/Program.cs
+++ puissance4/puissance4/Program.cs
@@ -6,18 +6,54 @@ namespace puissance4
     {
         static void Main(string[] args)
         {
-            string[,] grille = new string[6,7];//grille de jeux
-            bool player;//bool qui montre quelle joueur joue
+            string[,] grille = new string[6, 7];//grille de jeux
             int n;// numero de la colone dans laquelle le joueur veut rentrez son pion 
-            int compt=1;//compteur de coup joue  
-            outils outils = new outils();
-            outils.rempliGrille(out grille);
+            string replay;//pour rejouer
+            string n1 = "lol";
+            string n2 = "LOL";
+            if (n1==n2)
+            {
+                Console.WriteLine("noooooo");
+            }
+            else
+            {
+                Console.WriteLine("yesssss");
+            }
             do
             {
-                outils.affichgrille(grille);
-                Console.WriteLine(compt);
-                compt = compt + 1;
-            } while (compt<43);
+                bool player = true;//bool qui montre quelle joueur joue
+                bool verif = false;//bool qui montre que l'un des joueur a gagne
+                int compt = 1;//compteur de coup 
+                outils outils = new outils();
+                outils.rempliGrille(out grille);
+                do
+                {
+                    if (player == true)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("joueur 1");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("joueur 2");
+                    }
+                    outils.affichgrille(grille);
+                    outils.tryparse(out n);
+
+                    outils.choiPiRedOrYell(n, player, grille, out grille);
+                    outils.verifLigne(grille, out verif);
+                    if (verif==false)
+                    {
+                       outils.veriColone(grille, out verif);
+                    }
+                    compt = compt + 1;
+                    player = !player;
+                } while (compt < 43 && verif == false);
+                Console.WriteLine("voulezvous recomencez?");
+                Console.WriteLine("oui(y) non(n)");
+                replay = Console.ReadLine();
+            } while (replay == "y");
         }
     }
 }
--- puissance4/puissance4/outils.cs
+++ puissance4/puissance4/outils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace puissance4
@@ -8,12 +9,12 @@ namespace puissance4
     {
         public void rempliGrille(out string[,] grille)
         {
-            grille = new string[6,7];
+            grille = new string[6, 7];
             for (int i = 0; i < 6; i++)
             {
                 for (int j = 0; j < 7; j++)
 
-                    grille[i,j] = "0";
+                    grille[i, j] = "0";
 
             }
         }
@@ -23,8 +24,9 @@ namespace puissance4
             {
                 for (int j = 0; j < 7; j++)
                 {
-                    if (grille[i,j]=="0")
+                    if (grille[i, j] == "0")
                     {
+                        Console.ForegroundColor = ConsoleColor.White;
                         Console.Write(grille[i, j]);
                     }
                     else if (grille[i, j] == "O")
@@ -41,6 +43,163 @@ namespace puissance4
                 Console.WriteLine("");
             }
 
+        }
+        public void choiPiRedOrYell(int n, bool player, string[,] grille, out string[,] grille2)
+        {
+
+            if (player == true)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                string pionJoueur = "O";
+                string piontAdverse = "o";
+                posiPion(n, pionJoueur, piontAdverse, grille, out grille2);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                string pionJoueur = "o";
+                string piontAdverse = "O";
+                posiPion(n, pionJoueur, piontAdverse, grille, out grille2);
+            }
+
+
+        }
+        public void posiPion(int n, string pionjoueur, string piontAdverse, string[,] grille, out string[,] grille2)
+        {
+
+            int x = 5;
+            bool verif = false;
+            do
+            {
+                if (grille[x, n - 1] == "0")
+                {
+                    grille[x, n - 1] = pionjoueur;
+                    verif = true;
+                    //affichgrille(grille);
+                }
+                else if (x == 0 && (grille[x, n - 1] == "o" || grille[x, n - 1] == "O"))
+                {
+                    Console.WriteLine("vous ne pouvez pas mettre de pion dans cette colone");
+                }
+                else
+                {
+                    x = x - 1;
+                }
+            } while (verif == false);
+            grille2 = grille;
+        }
+        public void tryparse(out int n)
+        {
+            bool trypasse;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("dans quel colone voulez vous mettre votre pion");
+            do
+            {
+
+                trypasse = false;
+
+                if (int.TryParse(Console.ReadLine(), out n))
+                {
+                    if (n < 0 || n > 7)
+                    {
+
+                        Console.WriteLine("ERROR ENTREE INCORRECT veuillez entrez une numero de colone  correct");
+                    }
+                    else
+                    {
+                        trypasse = true;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERROR ENTREE INCORRECT veuillez entrez une valeur correct");
+                }
+            } while (trypasse == false);
+
+        }
+        public void verifLigne(string[,] grille, out bool verifi)
+        {
+            bool verif = false;
+            int i;
+            int x = 0;
+            do
+            {
+                i = 0;
+                do
+                {
+
+                    if (grille[x, i] != "0" && ((grille[x, i] == grille[x, i + 1]) && (grille[x, i + 1] == grille[x, i + 2]) && (grille[x, i + 2] == grille[x, i + 3])))
+                    {
+                        verif = true;
+                        Console.WriteLine("noooooo");
+                    }
+                    else
+                    {
+                        i = i + 1;
+                        Console.WriteLine("no");
+                    }
+                } while (verif == false && i < 3);
+                x = x + 1;
+            } while (verif == false && x < 6);
+
+
+            verifi = verif;
+        }
+        public void veriColone(string[,] grille, out bool verifi)
+        {
+            bool verif = false;
+            int i = 0;
+            int x;
+            do
+            {
+                x = 0;
+                do
+                {
+
+                    if (grille[x, i] != "0" && ((grille[x, i] == grille[x + 1, i]) && (grille[x + 1, i] == grille[x + 2, i]) && (grille[x + 2, i] == grille[x + 3, i])))
+                    {
+                        verif = true;
+                        Console.WriteLine("noooooo");
+                    }
+                    else
+                    {
+                        x = x + 1;
+                        Console.WriteLine("no");
+                    }
+                } while (verif == false && x < 3);
+                i = i + 1;
+            } while (verif == false && i < 5);
+
+
+            verifi = verif;
+        }
+        public void verifDiago(string[,] grille, out bool verifi)
+        {
+            bool verif = false;
+            int i = 0;
+            int x;
+            do
+            {
+                x = 0;
+                do
+                {
+
+                    if (grille[x, i] != "0" && ((grille[x, i] == grille[x + 1, i+1]) && (grille[x + 1, i+1] == grille[x + 2, i+2]) && (grille[x + 2, i+2] == grille[x + 3, i+3])))
+                    {
+                        verif = true;
+                        Console.WriteLine("noooooo");
+                    }
+                    else
+                    {
+                        x = x + 1;
+                        Console.WriteLine("no");
+                    }
+                } while (verif == false && x < 3);
+                i = i + 1;
+            } while (verif == false && i < 5);
+
+
+            verifi = verif;
         }
     }
 }
